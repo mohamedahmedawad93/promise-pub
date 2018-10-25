e6574bf24372f417230a564eca9e4265b0a20108
Mohamed Awad <mohamedahmedawad93@gmail.com>
2018-10-25 23:36:27 +0200

    added reader file

---
+++ reader.py
@@ -0,0 +1,30 @@
+"""
+This is the reader module
+It implements the following functions 
+	- `read_data`: gen. :
+		Responsible for reading a headerless csv file in the format `Full Name,e-mail` and yielding each row
+"""
+import csv
+
+
+def read_data(file_name):
+	"""
+	This is a generator function
+
+	reads a headerless comma delimited csv file in the format
+		Full name,e-mail
+
+	input: file_name : string
+	yields:
+		name(string), e-mail(string)
+	"""
+	with open(file_name, 'r') as data_file:
+		data = csv.reader(data_file, delimiter=',', quotechar='"')
+		for row in data:
+			try:
+				# just making sure that the row complies to the assumed format
+				name, mail = row
+				yield name, mail
+			except ValueError as _:
+				# whoops, wrong format (Python couldn't unpack the 2 values). ignore and continue
+				continue
